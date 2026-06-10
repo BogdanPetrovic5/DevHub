b0192d2ac5e6d22834355594df75f2ba2371e270
Bogdan <petrovic215@gmail.com>
2026-06-10 21:33:52 +0200

    Backend: Add swagger

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -9,13 +9,16 @@ builder.Services.AddDbContext<DevHubDbContext>(options => { options.UseSqlServer
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
