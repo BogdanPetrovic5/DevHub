5e4e164730bbc6712e9e08615e1c7bcbc6c999fb
Bogdan <petrovic215@gmail.com>
2026-06-10 20:34:13 +0200

    Backend: Initial DB creation and connection, add user model

---
+++ Backend/Backend/Data/DevHubDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class DevHubDbContext : DbContext
+    {
+        public DevHubDbContext(DbContextOptions<DevHubDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+        }
+    }
+}
---
+++ Backend/Backend/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string PasswordHash { get; set; } = String.Empty;
+
+    }
+}
