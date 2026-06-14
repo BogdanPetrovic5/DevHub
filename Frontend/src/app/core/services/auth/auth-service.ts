762290db0a98503502f7792c8692aa4f2d0091db
Bogdan <petrovic215@gmail.com>
2026-06-14 22:09:10 +0200

    feat: implement registration form with validation

---
+++ Frontend/src/app/core/services/auth/auth-service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Service } from '@angular/core';
+import { AuthResponse, RegisterRequest } from '../../models/auth.model';
+import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Service()
+export class AuthService {
+    private _httpClient = inject(HttpClient);
+
+    register(registerData:RegisterRequest):Observable<AuthResponse>{
+        const url = `${environment.apiUrl}/api/auth/register`
+        
+        return this._httpClient.post<AuthResponse>(url, registerData)
+    }
+}
