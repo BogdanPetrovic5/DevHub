20c7314b6df120f9616b452dfe27792ad8d7d8d5
Bogdan <petrovic215@gmail.com>
2026-06-16 17:37:01 +0200

    feat: add rememberMe field in refreshToken

---
+++ Backend/Backend/Migrations/20260616153626_Add_RememberMe_Field_RefreshToken.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_RememberMe_Field_RefreshToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "RememberMe",
+                table: "RefreshTokens",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RememberMe",
+                table: "RefreshTokens");
+        }
+    }
+}
