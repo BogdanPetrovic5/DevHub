a70749f7bf7fbdffbaf0d9604f8f3dc66b95d297
Bogdan <petrovic215@gmail.com>
2026-06-16 17:59:00 +0200

    feat: dashboard and auth guard

---
+++ Frontend/src/app/core/interceptors/auth-interceptor.ts
@@ -0,0 +1,27 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { AuthService } from '../services/auth/auth-service';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, switchMap, throwError } from 'rxjs';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return next(req).pipe(
+    catchError(error => {
+      if (error.status === 401 && !req.url.includes('/auth/refresh')) {
+        return authService.refresh().pipe(
+          switchMap(() => next(req)),
+          catchError(refreshError => {
+            authService.logout().subscribe();
+            router.navigate(['/auth']);
+            
+            return throwError(() => refreshError);
+          })
+        );
+      }
+      return throwError(() => error);
+    })
+  );
+};
