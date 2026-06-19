a1b05210b2f0fee175ddb64401b61f476258e546
Bogdan <petrovic215@gmail.com>
2026-06-19 18:30:29 +0200

    feat: add repo ui display

--- Frontend/src/app/core/services/repository/repo-service.ts
+++ Frontend/src/app/core/services/repository/repo-service.ts
@@ -1,15 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { Observable } from 'rxjs';
-import { CreateRepoRequest, RepoResponse } from '../../models/repo.model';
+import { CreateRepoRequest, RepoDto, RepoResponse } from '../../models/repo.model';
 import { environment } from '../../../../environments/environment';
 
 @Service()
 export class RepoService {
     private _httpClient = inject(HttpClient);
 
-    submit(data:CreateRepoRequest):Observable<RepoResponse>{
+    submit(data: CreateRepoRequest): Observable<RepoResponse> {
         const url = `${environment.apiUrl}/api/repo/new`
-        return this._httpClient.post<RepoResponse>(url, data, {withCredentials:true})
+        return this._httpClient.post<RepoResponse>(url, data, { withCredentials: true })
+    }
+
+    getUserRepos(): Observable<RepoDto[]> {
+        const url = `${environment.apiUrl}/api/repo/user`
+        return this._httpClient.get<RepoDto[]>(url, { withCredentials: true })
     }
 }
