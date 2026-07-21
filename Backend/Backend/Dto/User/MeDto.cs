89cb1afbec37bd57ea5c61712982d24cc76c251f
Bogdan <petrovic215@gmail.com>
2026-07-21 15:28:53 +0200

    Merge branch 'main' of github.com:BogdanPetrovic5/DevHub into frontend

---
+++ Backend/Backend/Dto/User/MeDto.cs
@@ -0,0 +1,8 @@
+namespace Backend.Dto.User
+{
+    public class MeDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
