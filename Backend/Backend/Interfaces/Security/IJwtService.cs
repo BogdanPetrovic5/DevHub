5669d867cb1e32ec1a2aa6e812089e91a229ace3
Bogdan <petrovic215@gmail.com>
2026-07-31 17:13:17 +0200

    feat: cli access token lasts longer

--- Backend/Backend/Interfaces/Security/IJwtService.cs
+++ Backend/Backend/Interfaces/Security/IJwtService.cs
@@ -4,7 +4,7 @@ namespace Backend.Interfaces.Security
 {
     public interface IJwtService
     {
-        string GenerateAccessToken(Models.User user);
+        string GenerateAccessToken(Models.User user, bool isCli = false);
         string GenerateRefreshToken();
     }
 }
