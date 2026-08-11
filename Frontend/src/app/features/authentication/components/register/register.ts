34fc2631a60d0364481dcf0f77e523f0d0db0a22
Bogdan <petrovic215@gmail.com>
2026-08-11 13:44:08 +0200

    feat: show errors from data annotations

--- Frontend/src/app/features/authentication/components/register/register.ts
+++ Frontend/src/app/features/authentication/components/register/register.ts
@@ -1,7 +1,9 @@
 import { Component, inject, output, signal } from '@angular/core';
-import { AbstractControl, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { AuthService } from '../../../../core/services/auth/auth-service';
 import { RegisterRequest } from '../../../../core/models/auth.model';
+import { Router } from '@angular/router';
+
 
 function passwordMatchValidator(form: AbstractControl) {
   const password = form.get('password')?.value;
@@ -20,16 +22,17 @@ export class Register {
 
   private _authService = inject(AuthService);
   private _fb = inject(FormBuilder);
-
+  private _router = inject(Router);
   isLoading = signal(false);
   errorMessage = signal<string | null>(null);
+  validationErrors = signal<ValidationErrors | null>(null);
 
   registerForm = this._fb.group({
     username: ['', Validators.required],
     email: ['', [Validators.required, Validators.email]],
     firstName: ['', Validators.required],
     lastName: ['', Validators.required],
-    password: ['', [Validators.required, Validators.minLength(6)]],
+    password: ['', [Validators.required]],
     confirmPassword: ['', Validators.required],
   }, { validators: passwordMatchValidator });
 
@@ -37,23 +40,36 @@ export class Register {
     return this.registerForm.hasError('passwordMismatch') &&
       !!this.registerForm.get('confirmPassword')?.touched;
   }
-
+  validationErrorValue(key:string): string | null {
+    const errors = this.validationErrors();
+    
+    return errors ? errors[key]?.[0] || null : null;
+  }
+  
   register() {
 
     if (this.registerForm.invalid) return;
+    this.validationErrors.set(null);
     this.isLoading.set(true);
     this.errorMessage.set(null);
 
     const { confirmPassword, ...registerData } = this.registerForm.value;
 
     this._authService.register(registerData as RegisterRequest).subscribe({
-      next: () => {
+      next: (response) => {
         this.isLoading.set(false);
+        if (response.success) {
+          this._router.navigate(['dashboard']);
+        }
       },
       error: (err) => {
-        console.log(err)
         this.isLoading.set(false);
-        this.errorMessage.set(err.error?.message ?? 'Registration failed');
+        if(err.error?.errors) {
+          this.validationErrors.set(err.error.errors);
+          console.log('Validation Errors:', err.error.errors);
+        } else {
+          this.errorMessage.set(err.error?.message || 'An error occurred during registration.');
+        }
       }
     });
   }
